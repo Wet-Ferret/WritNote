7b2e8c5f35cc72a9f1d473787f1958a964dcff5f
Ennis Massey <ennisbaradine@gmail.com>
2016-02-21 19:45:35 +1300

    Add whitespace ignore

--- lexer/lex.js
+++ lexer/lex.js
@@ -5,10 +5,17 @@ var lexer = new Lexer();
 
 
 var doLexer = function () {
+    // String handling
     var lexer = new Lexer();
     lexer.addRule(/[a-zA-Z0-9]+/, function (lexeme) {
         return arguments;
     }
     )
+    // Whitespace Ignore
+    lexer.addRule(/\S+/, function (lexeme) {
+
+    }
+    )
+
 }
 

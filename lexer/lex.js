97930290499c41428a863af6e6b1ad60afc5a6a2
Ennis Massey <ennisbaradine@gmail.com>
2016-02-21 19:41:03 +1300

    Fix return statement

--- lexer/lex.js
+++ lexer/lex.js
@@ -7,7 +7,7 @@ var lexer = new Lexer();
 var doLexer = function () {
     var lexer = new Lexer();
     lexer.addRule(/[a-zA-Z0-9]+/, function (lexeme) {
-        return lexeme;
+        return arguments;
     }
     )
 }
